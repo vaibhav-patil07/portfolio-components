7df1f47a5098b3bbd1e316e35bab6ecbb197e761
Vaibhav Patil <vaibhav.patil@contentstack.com>
2023-01-31 17:10:42 +0530

    Added styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import UniqueId from "../utils/UniqueId.js";
 import Component from "../components/Component.js";
 
 class Header extends Component {
+  displayFlex = "display--flex";
+  flexJustifyContentCenter = "flex--justify-c-center";
+  flexAlignItemCenter = "flex--align-i-center";
   constructor(data) {
     super();
     this.logo = data.logo;
@@ -19,10 +22,30 @@ class Header extends Component {
       const navLink = document.createElement("a");
       navLink.innerText = element.title;
       navLink.href = element.href;
+      navLink.classList.add("nav__item");
       nav.appendChild(navLink);
     });
     logo.src = this.logo;
     title.innerText = this.title;
+
+    title.classList.add("header__title");
+    container.classList.add(
+      "header",
+      this.displayFlex,
+      this.flexAlignItemCenter
+    );
+    logoNameContainer.classList.add(
+      "header__logo-name-container",
+      this.displayFlex,
+      this.flexAlignItemCenter
+    );
+    nav.classList.add(
+      "header__nav",
+      this.displayFlex,
+      this.flexAlignItemCenter,
+      this.flexJustifyContentCenter
+    );
+    logo.classList.add("header__logo");
 
     logoNameContainer.appendChild(logo);
     logoNameContainer.appendChild(title);
