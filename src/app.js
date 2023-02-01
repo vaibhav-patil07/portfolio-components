53ef90d0a9895cef5f4fdbb8c25698bfd5cd73c4
Vaibhav Patil <vaibhav.patil@contentstack.com>
2023-02-01 15:16:28 +0530

    Added project and its styling

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import headerData from "../data/header.json" assert { type: "json" };
 import homeData from "../data/home.json" assert { type: "json" };
 import aboutData from "../data/about.json" assert { type: "json" };
+import projectData from "../data/project.json" assert { type: "json" };
 import { Header } from "./components/Header.js";
 import { Home } from "./components/Home.js";
 import { About } from "./components/About.js";
+import { Project } from "./components/Project.js";
 
 const domHeader = document.querySelector("header");
 const domMain = document.querySelector("main");
@@ -16,3 +18,6 @@ myHome.mount(domMain);
 
 const myAbout = new About(aboutData);
 myAbout.mount(domMain);
+
+const myProject = new Project(projectData);
+myProject.mount(domMain);
---
+++ src/components/Project.js
@@ -0,0 +1,49 @@
+import UniqueId from "../utils/UniqueId.js";
+import Component from "../components/Component.js";
+import { Card } from "../components/Card.js";
+
+class Project extends Component {
+  constructor(data) {
+    super();
+    this.data = data;
+  }
+  render() {
+    const container = document.createElement("div");
+    const title = document.createElement("div");
+
+    container.classList.add(
+      "projects",
+      this.displayFlex,
+      this.flexDirectionCol,
+      this.flexAlignItemCenter
+    );
+    title.classList.add("about__title");
+
+    title.innerText = this.data.title;
+    container.appendChild(title);
+    const projectCards = new Array();
+    this.data.projects.forEach((project) => {
+      projectCards.push(new Card(project));
+    });
+    let i = 0;
+    while (i < projectCards.length) {
+      const row = document.createElement("div");
+      row.classList.add(
+        "project__cards",
+        this.displayFlex,
+        this.flexJustifyContentCenter
+      );
+      projectCards[i].mount(row);
+      i++;
+      if (i < projectCards.length) {
+        projectCards[i].mount(row);
+      }
+      i++;
+      container.appendChild(row);
+    }
+
+    return container;
+  }
+}
+
+export { Project };
